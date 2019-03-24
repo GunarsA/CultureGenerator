162d152abfb881dbe301c7e843dee9e0d276ae88
Gunārs Ābeltiņš <48802417+GunarsA@users.noreply.github.com>
2019-03-24 20:07:52 +0200

    Dizaina update

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,6 +1,8 @@
 function toggleSidebar(){
     document.getElementById("sidebar").classList.toggle("active");
     document.getElementById("heading_holder").classList.toggle("sidebarOpenMain");
+    document.getElementById("map").classList.toggle("sidebarOpenMain");
+    document.getElementById("sidebar_toggle_btn").classList.toggle("sidebarOpenMain");
 }
 function updateTextInput(slider){
     var output=document.getElementById("currentValue");
