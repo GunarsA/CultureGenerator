0e2d4c707299ebbe33dd72d21555e303dd7046ff
Gunārs Ābeltiņš <48802417+GunarsA@users.noreply.github.com>
2019-03-24 20:51:36 +0200

    Nekas īpašs

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -4,12 +4,5 @@ function toggleSidebar(){
     document.getElementById("map").classList.toggle("sidebarOpenMain");
     document.getElementById("sidebar_toggle_btn").classList.toggle("sidebarOpenMain");
 }
-function updateTextInput(slider){
-    var output=document.getElementById("currentValue");
-    output.innerHTML=slider;
-    slider.oninput=function(){
-        output.innerHTML=this.value;
-    }
-}
 
 
