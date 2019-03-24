6468d1f2a747133012775873cff762a3f578086f
KrisjanisP <krisjanispetrucena@gmail.com>
2019-03-24 17:52:16 +0200

    Karte
    Karte

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,36 +1,46 @@
 var map;
-var infowindow=new google.maps.InfoWindow();
+var infowindow;
+var markers = [];
+
 function update(){
-	dropPins(libraries);
-	var museum_bool = document.getElementById("museum_checkbox").value;
+	for(var i=0;i<markers.length;i++){
+		marker[i].setMap(null);
+	}
+	var museum_bool = document.getElementById("museum_checkbox").checked;
 	if(museum_bool===true){
 		dropPins(libraries);
 	}
 }
+
 function initMap() {
-	// lat lng
 	var myLatlng = new google.maps.LatLng(56.946285,24.105078);
 	var mapOptions = {
 		zoom: 8,
 		center: myLatlng
 	};
 	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	infowindow =new google.maps.InfoWindow();
 	update();
 }
-function setContent(marker, x,infowindow){
+
+function setContent(marker, location, infowindow){
 	return function() {
-		infowindow.setContent(x.Name);
+		infowindow.setContent(location.Name);
 		infowindow.open(map, marker);
 	};
 }
+function addMarker(location) {
+	var myLatlng = new google.maps.LatLng(parseFloat(location.LAT), parseFloat(location.LON));
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+		icon: "assets/img/library_resized.png"
+    });
+    markers.push(marker);
+	google.maps.event.addListener(marker, 'click', setContent(marker, location,infowindow));
+}
 function dropPins(allPoints){
 	for (var i = 0; i < allPoints.length; i++){
-		var myLatlng = new google.maps.LatLng(parseFloat(allPoints[i].LAT), parseFloat(allPoints[i].LON));
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map,
-			icon: "assets/img/library_resized.png"
-		});
-		google.maps.event.addListener(marker, 'click', setContent(marker, allPoints[i],infowindow));  
+		addMarker(allPoints[i]);  
 	}
 }
