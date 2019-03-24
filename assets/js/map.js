d58e9eaa3b94321b37229506048c3a3407f0d363
KrisjanisP <krisjanispetrucena@gmail.com>
2019-03-24 15:29:39 +0200

    Karte

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,30 +1,35 @@
+var map;
+function update(){
+	dropPins(libraries);
+}
 function initMap() {
+	// lat lng
 	var myLatlng = new google.maps.LatLng(56.946285,24.105078);
 	var mapOptions = {
 		zoom: 7.5,
 		center: myLatlng
 	};
-	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-	dropPins(libraries, map);
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	update();
 }
 function sayHi(){
 		alert("hi");
 }
-function dropPins(allPoints, map){
+function setContent(marker, x,infowindow){
+	return function() {
+		infowindow.setContent(x.Name);
+		infowindow.open(map, marker);
+	};
+}
+function dropPins(allPoints){
 	var infowindow = new google.maps.InfoWindow(); 
 	for (var i = 0; i < allPoints.length; i++){
 		var myLatlng = new google.maps.LatLng(parseFloat(allPoints[i].LAT), parseFloat(allPoints[i].LON));
 		var marker = new google.maps.Marker({
 			position: myLatlng,
 			map: map,
-			icon: "assets/img/library_resized.png",
-			animation: google.maps.Animation.DROP
+			icon: "assets/img/library_resized.png"
 		});
-		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-		return function() {
-		infowindow.setContent(allPoints[i].Name);
-		infowindow.open(map, marker);
-		};
-		})(marker, i));  
+		google.maps.event.addListener(marker, 'click', setContent(marker, allPoints[i],infowindow));  
 	}
 }
